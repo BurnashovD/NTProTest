a39d4aa22e48d401fd4d7130b34e6ae70a55d063
Daniil Burnashov <danil-burnashov@mail.ru>
2023-03-14 21:25:47 +0300

    Закончил с UI

---
+++ NTProTest/NTProTest/FiltersView.swift
@@ -0,0 +1,93 @@
+//
+//  FiltersView.swift
+//  NTProTest
+//
+//  Created by Daniil on 14.03.2023.
+//
+
+import UIKit
+
+final class FiltersView: UIView {
+    
+    private let filtersStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .vertical
+        stack.spacing = 1
+        stack.alignment = .leading
+        stack.distribution = .equalCentering
+        return stack
+    }()
+    
+    private let doneButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Done", for: .normal)
+        button.setTitleColor(.tintColor, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let titles = ["Дата изменения сделки", "Имя инструмента", "Цена сделки", "Объем сделки", "Сторона сделки"]
+    
+    var parameterHandler: (() -> Void)?
+    var dismissHandler: (() -> Void)?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        createStackViewAnchors()
+        createDoneButtonAnchors()
+    }
+    
+    private func configureUI() {
+        addSubviewsToStack()
+        addSubview(filtersStackView)
+        addSubview(doneButton)
+        doneButton.addTarget(self, action: #selector(dismissAction), for: .touchUpInside)
+    }
+    
+    private func addSubviewsToStack() {
+        (0...4).forEach { index in
+            let button = UIButton()
+            button.setTitle(titles[index], for: .normal)
+            button.titleLabel?.font = .systemFont(ofSize: 15)
+            button.setTitleColor(.black, for: .normal)
+            button.tag = index
+            button.addTarget(self, action: #selector(changeFilterAction(sender:)), for: .touchUpInside)
+            filtersStackView.addArrangedSubview(button)
+        }
+    }
+    
+    private func createDoneButtonAnchors() {
+        NSLayoutConstraint.activate([
+            doneButton.topAnchor.constraint(equalTo: self.topAnchor, constant: 15),
+            doneButton.widthAnchor.constraint(equalToConstant: 65),
+            doneButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10)
+        ])
+    }
+    
+    private func createStackViewAnchors() {
+        NSLayoutConstraint.activate([
+            filtersStackView.topAnchor.constraint(equalTo: self.topAnchor, constant: 50),
+            filtersStackView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            filtersStackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10),
+            filtersStackView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -50)
+        ])
+    }
+    
+    @objc private func changeFilterAction(sender: UIButton) {
+        parameterHandler?()
+    }
+    
+    @objc private func dismissAction() {
+        dismissHandler?()
+    }
+}
---
+++ NTProTest/NTProTest/HeaderCell.swift
@@ -0,0 +1,69 @@
+//
+//  HeaderCell.swift
+//  NTProTest
+//
+//  Created by Daniil on 14.03.2023.
+//
+
+import UIKit
+
+final class HeaderCell: UITableViewHeaderFooterView {
+    // MARK: - Visual components
+    
+    private let parametersStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.distribution = .equalCentering
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    // MARK: - Public properties
+    
+    static let identifier = "headerCell"
+    
+    // MARK: - Private properties
+    
+    private let titles = ["Instruments", "Price", "Amount", "Side"]
+    
+    // MARK: - init
+    
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(parametersStackView)
+        addArrangeSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Public methods
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        createStackViewAnchors()
+    }
+    
+    // MARK: - Private methods
+    
+    private func addArrangeSubviews() {
+        (0...3).forEach { index in
+            let label = UILabel()
+            label.font = .systemFont(ofSize: 14)
+            label.textColor = .black
+            label.text = titles[index]
+            parametersStackView.addArrangedSubview(label)
+        }
+    }
+    
+    private func createStackViewAnchors() {
+        NSLayoutConstraint.activate([
+            parametersStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            parametersStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            parametersStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            parametersStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
+}
+
+
